4a047dc2457a9eab0c8f5f4275225a0e29834e44
git <farhad.jalali@gmail.com>
2020-04-19 18:47:50 +0300

    fix reorder bug

--- src/types.js
+++ src/types.js
@@ -18,7 +18,7 @@ class AuditType {
 exports.AuditType = AuditType;
 class Global {
     constructor() {
-        this.dbs = {};
+        this.dbs = {}; // any not mongodb.Db because of client side reference
         this.packageInfo = {};
         this.packageConfig = {};
         this.clientQuestionCallbacks = {};
@@ -231,6 +231,7 @@ var PanelType;
 (function (PanelType) {
     PanelType[PanelType["Stack"] = 1] = "Stack";
     PanelType[PanelType["Dock"] = 2] = "Dock";
+    // Grid = 3,
     PanelType[PanelType["Wrap"] = 4] = "Wrap";
     PanelType[PanelType["Flex"] = 5] = "Flex";
     PanelType[PanelType["Modal"] = 6] = "Modal";
